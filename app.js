abe644f1dcfc047afccc1f755d212aaea117aa72
srujan <srujan001ssa@gmail.com>
2021-09-14 17:24:56 +0530

    added style and API

--- app.js
+++ app.js
@@ -2,10 +2,29 @@ var btnTranslate= document.querySelector("#btn-translate");
 var txtInput= document.querySelector("#txt-input");
 var outputDiv= document.querySelector("#output");
 
-outputDiv.innerText="srujan"
+//(This is the mock server URL that sir made for practice)https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json
+
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
+
+function getTranslationURL(text){
+  return serverURL + "?" + "text=" + text;
+}
+
+function errorHandler(error){
+  console.log("encountered error: ",error)
+  alert("Server seems to be experiencing issues, try again later")
+}
 
 function clickHandler(){
-  console.log("clicked");
-    console.log("Input: ",txtInput.value);
+  var inputTxt = txtInput.value //taking input
+
+  fetch(getTranslationURL(inputTxt))
+  .then(response => response.json())
+  .then(json => {
+    var translatedText = json.contents.translated
+    outputDiv.innerText = translatedText
+  })
+  .catch(errorHandler)
 }
-btnTranslate.addEventListener("click",clickHandler);
+
+btnTranslate.addEventListener("click",clickHandler)
